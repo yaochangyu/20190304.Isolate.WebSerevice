190f7f8455d9a625c8df4e90530ae36634aa6976
余小章 <yao-chang.yu@outlook.com>
2019-03-04 23:22:27 +0800

    調用簽核狀態_預期得到開立，紅燈

--- Lab/Lab.DAL.UnitTest/UnitTest1.cs
+++ Lab/Lab.DAL.UnitTest/UnitTest1.cs
@@ -22,6 +22,15 @@ namespace Lab.DAL.UnitTest
             var status = repository.GetStatus();
             Assert.AreEqual("暫止",status);
         }
+
+        [TestMethod]
+        public void 調用簽核狀態_預期得到開立()
+        {
+            var repository = new ApproveRepository();
+            repository.Adapter = new FakeApproveAdapter();
+            var status = repository.GetStatus();
+            Assert.AreEqual("開立",status);
+        }
     }
 
     public class FakeApproveAdapter : IApproveAdapter
